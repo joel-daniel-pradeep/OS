e19990ff6f28ac546ae4ef28532db5373fb88722
joel-daniel-pradeep <joelmavileth@gmail.com>
2024-06-25 17:32:28 +0530

    Create Dinphil.c

---
+++ Dinphil.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<semaphore.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+sem_t c[5];
+pthread_t p[5];
+
+void *dine(void *args){
+   int i=*((int *)args);
+   printf("P%d is hungry\n",i+1);
+   sem_wait(&c[i]);
+   sem_wait(&c[(i+1)%5]);
+   printf("P%d has acquired both the forks %d and %d\n",i+1,i,(i+1)%5);
+   sleep(2);
+   sem_post(&c[i]);
+   sem_post(&c[(i+1)%5]);
+   printf("released both the chopsticks:\n");
+}
+
+void main()
+{
+  int i,a[5];
+  for(i=0;i<5;i++){
+    sem_init(&c[i],0,1);
+    a[i]=i;
+  }
+  for(i=0;i<5;i++){
+    pthread_create(&p[i],NULL,(void *)dine,(void *)&a[i]);
+  }
+  for(i=0;i<5;i++){
+    pthread_join(p[i],NULL);
+  }
+  for(i=0;i<5;i++){
+   sem_destroy(&c[i]);
+  }
+ }
